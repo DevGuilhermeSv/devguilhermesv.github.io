fbad1aa487bfa8e98c7fa464d216ee4d565b4f7d
Guilherme Silva <gui.2469@gmail.com>
2021-03-08 03:24:38 -0300

    Atualização - 8/03/2020

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 $(function(){
   
-       var time = 5;
+       var time = 12000;
         $('.anime').each(function(){
             
             setTimeout(()=>{
@@ -10,6 +10,59 @@ $(function(){
             
             
         }) 
-        console.log('ok');
+        /* configurações para efeito de digitação------------ */
+        var textoInicial = $("#textoIncial").children();
+        var filhos = BuscaFilho(textoInicial);
+        for (let index = 0; index < filhos.length; index++) {
+            const element = filhos[index];
+
+            typeWrite(element);
+            
+        }
+        $('.add-fade').attr('class','anime');
+       
+       
+        
+        
 
 });
+ /* Retorna um array com os elementos filhos de um elemento Pai */
+ function BuscaFilho(textoInicial) {
+    var filhos =[];
+    for (let index = 0; index < textoInicial.length; index++) {
+        const element = textoInicial[index];
+        filhos.push(element); 
+       
+        
+    }
+    return filhos;
+      
+}
+/* Adiciona o efeito de digitação no texto do elemento */
+ function typeWrite(elemento){
+
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    
+    
+    for (let index = 0; index < textoArray.length; index++) {
+        const letra = textoArray[index];
+        
+            setTimeout(function(){
+                elemento.innerHTML += letra;
+                if(index == 0){
+                    $(elemento).attr('class','titulo-principal');
+                }
+               if(index+1 == textoArray.length){
+                $(elemento).removeAttr('class','titulo-principal');
+               }
+                 
+            }, 150 * indexador)
+        indexador++;
+        
+    }
+   
+    
+}
+/* Importante para contagem correta do TimeOut */
+var indexador=0;
