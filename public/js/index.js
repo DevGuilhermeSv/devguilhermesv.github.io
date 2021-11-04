2d5b58b07ade483d414e310e3f8cf5b54f212f24
Guilherme <gui.2469@gmail.com>
2021-11-03 23:36:35 -0300

    Added table of courses

--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,7 @@ $(function(){
         $('.add-fade').attr('class','anime');
        
        
-        
+        generateTableOfCurse();
         
 
 });
@@ -62,6 +62,62 @@ $(function(){
         
     }
    
+    
+}
+/**Preenche a tabela de Cursos */
+function generateTableOfCurse(){
+    var cursosAcademicos = `INTRODUÇÃO À CIÊNCIA DA COMPUTAÇÃO,
+                            FUNDAMENTOS DE PROGRAMAÇÃO,
+                            ARQUITETURA DE COMPUTADORES,
+                            MATEMÁTICA BÁSICA,
+                            PRÉ-CÁLCULO,
+                            PROGRAMAÇÃO ORIENTADA A OBJETOS,
+                            SISTEMAS OPERACIONAIS,
+                            ESTRUTURA DE DADOS,
+                            MATEMÁTICA DISCRETA,
+                            PROGRAMAÇÃO FUNCIONAL,
+                            LINGUAGENS FORMAIS E AUTÔMATOS,
+                            LÓGICA PARA COMPUTAÇÃO,
+                            FUNDAMENTOS DE BANCO DE DADOS,
+                            LINGUAGENS DE PROGRAMAÇÃO,
+                            REDES DE COMPUTADORES,
+                            ENGENHARIA DE SOFTWARE,
+                            INTERAÇÃO HUMANO-COMPUTADOR,
+                            SISTEMAS DISTRIBUÍDOS,
+                            ESTÁGIO SUPERVISIONADO I`;
+    var cursosAlternaticos =[ {disciplina: 'DOCKER',
+                                status: 'Concluido'},
+                                {disciplina: 'Dotnet Core Basico',
+                                status: 'Concluido'},
+                                {disciplina: 'PRINCIPIOS BASICOS DE PENTEST',
+                                status: 'Concluido'},
+                                {disciplina: 'DEVOPS NINJA',
+                                status: 'Pentente'}]
+    
+    
+    var cursosArray = cursosAcademicos.split(',');
+    cursosArray.forEach(element => {
+        var htmlSyntax = `<tr>
+                            <th scope="row">Academica</th>
+                            <td>${element}</td>
+                            <td> <div class="d-inline-flex ">
+                            <div class="fa fa-check-circle align-self-center ml-2" style="font-size:12px;color:green"> </div>
+                        </div></td>
+                            
+                        </tr>`
+        $('#table-course-body').append(htmlSyntax)
+    });
+    cursosAlternaticos.forEach(element => {
+        var htmlSyntax = `<tr>
+                            <th scope="row">Externa</th>
+                            <td>${element.disciplina}</td>
+                            <td> <div class="d-inline-flex ">
+                            <div class="fa fa-check-circle align-self-center ml-2" style="font-size:12px;color:${element.status =="Concluido"?"green":"yellow"}"> </div>
+                        </div></td>
+                            
+                        </tr>`
+        $('#table-course-body').append(htmlSyntax)
+    });
     
 }
 /* Importante para contagem correta do TimeOut */
