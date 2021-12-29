e5238268e1532f102ec6ba9f5c1abcfed31381f0
Guilherme Rodrigues <gui_s@DESKTOP-H4ERN8E>
2021-12-29 15:13:37 -0300

    adicionado projetos

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,15 +1,15 @@
-$("#menu > div.menu-sobre").on('click',()=>{
+$(".menu > div.menu-sobre").on('click',()=>{
     $.get('/sobre.html',(res)=>{
         $(".default").html(res);
     })
 })
-$("#menu > div.menu-projetos").on('click',()=>{
+$(".menu > div.menu-projetos").on('click',()=>{
     $.get('/projetos.html',(res)=>{
         $(".default").html(res);
     })
 })
 
-$("#menu > div.menu-experiencias").on('click',()=>{
+$(".menu > div.menu-experiencias").on('click',()=>{
     $.get('/experiencias.html',(res)=>{
         $(".default").html(res);
     })
